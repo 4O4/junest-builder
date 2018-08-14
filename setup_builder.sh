e5b2a5b77a6c5cf9a9343fa618c4a8de2dd66c21
Paweł K <pk.pawelo@gmail.com>
2018-08-14 22:28:45 +0200

    Update setup_builder.sh

--- setup_builder.sh
+++ setup_builder.sh
@@ -34,8 +34,6 @@ cd \${JUNEST_BUILDER}/tmp/yaourt
 curl -L -J -O -k "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yaourt"
 makepkg --noconfirm -sfc
 sudo pacman --noconfirm -U yaourt*.pkg.tar.xz
-yaourt -S --noconfirm droxi
-droxi
 EOF
 
 mkdir -p /home/builder/.ssh
