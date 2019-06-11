2efe15ad87613ae873109261c75da497daf0db1b
Paweł K <pk.pawelo@gmail.com>
2019-06-11 10:34:16 +0200

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -5,6 +5,9 @@
 # that need to be deleted in dropbox
 ARCH=""
 [ "$1" != "" ] && ARCH=$1
+VARIANT="lighter"
+[ "$2" != "" ] && VARIANT=$2
+
 
 set -eu
 
@@ -26,7 +29,7 @@ sudo systemctl start haveged
 # Building JuNest image
 cd ${JUNEST_BUILDER}
 
-if [[ $2 == "lighter" ]]; then
+if [[ "${VARIANT}" == "lighter" ]]; then
   packages="zsh glibc gcc ranger exa vim python3 fasd aur:nnn thefuck tree fd progress ripgrep gawk aur:hstr mercurial cmake shellcheck diff-so-fancy tldr unp tmux grep unzip rsync zssh mc rclone glances nload the_silver_searcher httpie speedtest-cli aur:urlview uriparser jq aur:gqlplus fortune-mod cowsay lolcat ponysay go nodejs typescript gdb gtop"
 else
   packages="zsh glibc gcc ranger exa vim neovim python3 aur:aurman fasd aur:nnn thefuck tree fd progress ripgrep gawk aur:hstr mercurial cmake premake shellcheck diff-so-fancy tldr unp tmux grep unzip rsync zssh mc rclone glances nload netdata the_silver_searcher httpie speedtest-cli aur:urlview uriparser jq aur:gqlplus aur:pacvim-git fortune-mod cowsay lolcat ponysay go rust nodejs php composer typescript lua51 luajit clang gdb aur:python-flask12 aur:python-flask-compress aur:python-flask-socketio2 aur:python-pygdbmi aur:python-gdbgui gtop"
